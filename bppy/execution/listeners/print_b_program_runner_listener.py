1a2ced4aaada0f315710b6c6bd852d1d8e708524
adielas <adiel.ashrov@gmail.com>
2023-08-25 16:12:37 +0300

    Merge listeners/print_b_program_runner_listener.py from master to constraints branch.

--- bppy/execution/listeners/print_b_program_runner_listener.py
+++ bppy/execution/listeners/print_b_program_runner_listener.py
@@ -2,10 +2,17 @@ from bppy.execution.listeners.b_program_runner_listener import BProgramRunnerLis
 
 
 class PrintBProgramRunnerListener(BProgramRunnerListener):
+    """
+    This class implements the :class:`BProgramRunnerListener
+    <bppy.execution.listeners.b_program_runner_listener.BProgramRunnerListener>` interface, providing basic print
+    statements for specific events in the BProgram's execution.
+    """
 
     def starting(self, b_program):
-        # print("STARTED")
-        pass
+        """
+        Prints "STARTED" when the BProgram execution is about to start.
+        """
+        print("STARTED")
 
     def started(self, b_program):
         pass
@@ -14,8 +21,10 @@ class PrintBProgramRunnerListener(BProgramRunnerListener):
         pass
 
     def ended(self, b_program):
-        #print("ENDED")
-        pass
+        """
+        Prints "ENDED" when the BProgram execution is about to start.
+        """
+        print("ENDED")
 
     def assertion_failed(self, b_program):
         pass
@@ -30,13 +39,16 @@ class PrintBProgramRunnerListener(BProgramRunnerListener):
         pass
 
     def event_selected(self, b_program, event):
-        # print(event)
-        pass
+        """
+        Prints the selected event when an event has been selected during the BProgram's execution.
+        """
+        print(event)
 
     def halted(self, b_program):
         pass
 
     def __init__(self):
+        """
+        Initializes the PrintBProgramRunnerListener.
+        """
         super().__init__()
-
-
