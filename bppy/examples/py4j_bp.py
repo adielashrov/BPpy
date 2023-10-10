042b13ab7e1e52a601e8a687958829608f808685
adielas <adiel.ashrov@gmail.com>
2023-10-10 12:55:04 +0300

    Add example for executing BPjs from python file

---
+++ bppy/examples/py4j_bp.py
@@ -0,0 +1,16 @@
+from py4j.java_gateway import JavaGateway
+import sys
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+gateway = JavaGateway.launch_gateway(classpath=os.path.join(dir_path, "BPjs-0.12.3.uber.jar"),
+                                     die_on_exit=True,
+                                     redirect_stdout=sys.stdout)
+
+with open("HelloBPjsWorld.js", "r") as f:
+    bprog = gateway.jvm.il.ac.bgu.cs.bp.bpjs.model.StringBProgram("my_bprogram", f.read())
+
+runner = gateway.jvm.il.ac.bgu.cs.bp.bpjs.execution.BProgramRunner(bprog)
+runner.addListener(gateway.jvm.il.ac.bgu.cs.bp.bpjs.execution.listeners.PrintBProgramRunnerListener())
+runner.run()
